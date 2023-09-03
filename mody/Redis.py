f752b0ce4efe9878dffc10c331268d6a87482a98
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 17:44:33 +0300

    Update Redis.py

--- mody/Redis.py
+++ mody/Redis.py
@@ -1,5 +1,10 @@
+import os
 from redis import StrictRedis
-DATABASE_URL=${{Postgres.DATABASE_URL}}
-CACHE_HOST=${{Redis.REDISHOST}}
-PRISMA_DB=${{Postgres.DATABASE_URL}}?connection_limit=100
+
+# استخدم المتغيرات البيئية بدلاً من البنية التي قدمتها
+DATABASE_URL = os.environ.get('Postgres.DATABASE_URL')
+CACHE_HOST = os.environ.get('Redis.REDISHOST')
+PRISMA_DB = f'{DATABASE_URL}?connection_limit=100'
+
+# قم بإنشاء اتصال بقاعدة البيانات Redis باستخدام المتغيرات البيئية
 db = StrictRedis(decode_responses=True)
