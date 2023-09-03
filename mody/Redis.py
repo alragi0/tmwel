28e6d065b9f1bc3285d8d5f817589ba868791d2f
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 18:03:47 +0300

    Update Redis.py

--- mody/Redis.py
+++ mody/Redis.py
@@ -1,8 +1,3 @@
 from redis import StrictRedis
-import os
 
-# استخدم المتغير CACHE_HOST للحصول على عنوان خادم Redis من المتغيرات البيئية
-cache_host = os.environ.get("CACHE_HOST")
-
-# قم بتكوين اتصال قاعدة البيانات Redis باستخدام المتغير CACHE_HOST
-db = StrictRedis(host=cache_host, decode_responses=True)
+db = StrictRedis(decode_responses=True)
