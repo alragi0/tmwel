70e905f0b5a530fd1cfd4e1b7d65e9d6e1edcb64
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 17:42:34 +0300

    Update Redis.py

--- mody/Redis.py
+++ mody/Redis.py
@@ -1,3 +1,5 @@
 from redis import StrictRedis
-
+DATABASE_URL=${{Postgres.DATABASE_URL}}
+CACHE_HOST=${{Redis.REDISHOST}}
+PRISMA_DB=${{Postgres.DATABASE_URL}}?connection_limit=100
 db = StrictRedis(decode_responses=True)
