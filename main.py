f5168a2c7208e7b821a07d72ab2a03218c243beb
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-05 00:07:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import subprocess
 from pyrogram import idle
+
 import votlx
 from mody.Keyboards import start_key
 from mody.Redis import db
 from mody.get_info import sudo_info, get_bot
 from mody.mod import Bot, sudo_client
 
-def install_screen():
-    try:
-        subprocess.run(['sudo', 'apt-get', 'install', 'screen'])
-        print("تم تثبيت screen بنجاح")
-    except Exception as e:
-        print(f"حدث خطأ أثناء تثبيت screen: {str(e)}")
 
 async def main():
-    install_screen()  # تنفيذ تثبيت screen
     await Bot.start()
     if db.get(f'{Bot.me.id}:restart'):
         await Bot.send_message(sudo_info.username, '⌯ يمكنك استخدام البوت الان', reply_markup=start_key)
@@ -30,6 +23,7 @@ async def main():
     await idle()
     await Bot.stop()
     print("تم ايقاف البوت بنجاح")
+
 
 if __name__ == '__main__':
     votlx.client.loop.run_until_complete(main())
