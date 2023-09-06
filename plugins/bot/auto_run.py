be292322f2b68762a885528cee92a32a3f474731
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-06 23:13:57 +0300

    Update auto_run.py

--- plugins/bot/auto_run.py
+++ plugins/bot/auto_run.py
@@ -1,35 +1,47 @@
 import os
-import asyncio
-from asyncio.subprocess import PIPE
+
+from asyncio import create_task, create_subprocess_exec, subprocess, sleep
+
+
 
 from mody.Redis import db
+
 from mody.get_info import sudo_info
+
 from mody.mod import Bot
 
-async def run_session(session):
-    cmd = f"screen -d -m -S {session[:50]} python3 users.py {session}"
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=PIPE,
-        stderr=PIPE,
-        shell=True
-    )
-    await proc.communicate()
+
+
+
 
 async def auto_run():
-    while True:
-        for session in db.smembers(f'{Bot.me.id}:{sudo_info.id}:sessions'):
-            proc = await asyncio.create_subprocess_exec(
-                "screen",
-                "-ls",
-                stdout=PIPE,
-                stderr=PIPE,
-            )
-            stdout, stderr = await proc.communicate()
-            res = stdout.decode()
-            if session[:50] not in res:
-                asyncio.create_task(run_session(session))
-        await asyncio.sleep(10)
 
-if __name__ == "__main__":
-    asyncio.run(auto_run())
+    while not await sleep(10):
+
+        proc = await create_subprocess_exec(
+
+            "screen",
+
+            "-ls",
+
+            stdout=subprocess.PIPE,
+
+            stderr=subprocess.PIPE,
+
+        )
+
+        stdout, stderr = await proc.communicate()
+
+        res = stdout.decode()
+
+        for session in db.smembers(f'{Bot.me.id}:{sudo_info.id}:sessions'):
+
+            if session[:50] not in res:
+
+                os.system(f'screen -d -m -S {session[:50]} python3 users.py {session}')
+
+
+
+
+
+create_task(auto_run())
